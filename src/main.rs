4971024afdb3b27b69877a646c5b0b561d0f52cf
OskarMihalik <45460241+OskarMihalik@users.noreply.github.com>
2022-07-22 18:30:07 +0200

    feat: paddle baunce

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use ball::BallPlugin;
-use bevy::prelude::*;
-use components::{Ball, Movable, SpriteSize, Velocity};
+use bevy::{
+    math::Vec3Swizzles,
+    prelude::*,
+    sprite::collide_aabb::{collide, Collision},
+};
+use components::{Ball, Movable, Player, SpriteSize, Velocity};
 use player::PlayerPlugin;
 mod ball;
 pub mod components;
@@ -39,15 +43,12 @@ fn main() {
         .add_plugin(BallPlugin)
         .add_startup_system(setup_system)
         .add_system(movement_system)
+        .add_system(paddle_bounce)
         .run();
 }
 
-fn movement_system(
-    mut commands: Commands,
-    win_size: Res<WinSize>,
-    mut query: Query<(&mut Velocity, &mut Transform, &Movable)>,
-) {
-    for (mut velocity, mut transform, movable) in query.iter_mut() {
+fn movement_system(mut query: Query<(&Velocity, &mut Transform), With<Movable>>) {
+    for (velocity, mut transform) in query.iter_mut() {
         let translation = &mut transform.translation;
         let x = translation.x + velocity.x * TIME_STEP * BASE_SPEED;
         let y = translation.y + velocity.y * TIME_STEP * BASE_SPEED;
@@ -57,4 +58,34 @@ fn movement_system(
     }
 }
 
-// fn paddle_bounce(ball_query: Query<(&Transform, &mut Velocity, &SpriteSize), With<Ball>>, player_query: Query<>) {}
+fn paddle_bounce(
+    mut ball_query: Query<
+        (&mut Transform, &mut Velocity, &SpriteSize),
+        (With<Ball>, Without<Player>),
+    >,
+    player_query: Query<(&Transform, &SpriteSize), With<Player>>,
+) {
+    for (mut ball_transform, mut ball_velocity, ball_sprite_size) in ball_query.iter_mut() {
+        let ball_scale = Vec2::from(ball_transform.scale.xy());
+        for (player_transform, player_sprite_size) in player_query.iter() {
+            let player_scale = Vec2::from(ball_transform.scale.xy());
+
+            let collision = collide(
+                ball_transform.translation,
+                ball_scale * ball_sprite_size.0,
+                player_transform.translation,
+                player_scale * player_sprite_size.0,
+            );
+
+            if let Some(col) = collision {
+                match col {
+                    Collision::Right => ball_velocity.x *= -1.,
+                    Collision::Left => ball_velocity.x *= -1.,
+                    Collision::Top => ball_velocity.y *= -1.,
+                    Collision::Bottom => ball_velocity.y *= -1.,
+                    Collision::Inside => ball_transform.translation.x = 0.,
+                }
+            }
+        }
+    }
+}
